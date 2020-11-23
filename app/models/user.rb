f76034cfc892d4b7220286c3b1117aa3b27824aa
Juan <jacobo12.montes@gmail.com>
2020-11-23 17:49:38 -0500

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,27 +10,30 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array = friendships.map do |friendship|
+      friendship.friend if friendship.status
+    end + inverse_friendships.map do |friendship|
+            friendship.user if friendship.status
+          end
     friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map { |friendship| friendship.friend unless friendship.status }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.confirmed = true
+    friendship = inverse_friendships.find { |friendshipcheck| friendshipcheck.user == user }
+    friendship.status = true
     friendship.save
   end
 
